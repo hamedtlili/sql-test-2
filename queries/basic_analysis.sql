a2dfcd4ab6989cce58de39846afa43547f09b2e0
hamedtlili <hamed.tlili@esgitech.tn>
2026-04-08 00:14:29 +0200

    Update basic_analysis.sql

--- queries/basic_analysis.sql
+++ queries/basic_analysis.sql
@@ -6,138 +6,52 @@ FROM [MonEntreprise].[dbo].[Telco-Customer-Churn];
 SELECT[gender]
 FROM [MonEntreprise].[dbo].[Telco-Customer-Churn];
 
-
-
-
-
-
--- Understand who your customers are 
-
 USE [MonEntreprise]
 GO
 
-SELECT 
-    Churn,
-    AVG(CAST(MonthlyCharges AS FLOAT)) AS AvgCharge,
-    MIN(CAST(MonthlyCharges AS FLOAT)) AS MinCharge,
-    MAX(CAST(MonthlyCharges AS FLOAT)) AS MaxCharge
+-- afficher les 10 premières lignes
+SELECT TOP 10 *
+FROM [dbo].[Telco-Customer-Churn];
+
+-- nombre total de clients
+SELECT COUNT(*) AS TotalClients
+FROM [dbo].[Telco-Customer-Churn];
+
+-- clients churn / non churn
+SELECT Churn, COUNT(*) AS Total
 FROM [dbo].[Telco-Customer-Churn]
 GROUP BY Churn;
 
-
--- With churn insight
-
-USE [MonEntreprise]
-GO
-
-SELECT 
-    gender,
-    Churn,
-    COUNT(*) AS Total
-FROM [MonEntreprise].[dbo].[Telco-Customer-Churn]
-GROUP BY gender, Churn;
-
--- 2. Account Information Analysis
--- Identify risky customer profiles
-USE [MonEntreprise]
-GO
-
-SELECT 
-    CASE 
-        WHEN tenure < 12 THEN 'New'
-        WHEN tenure < 24 THEN 'Mid-term'
-        ELSE 'Loyal'
-    END AS CustomerType,
-    Churn,
-    COUNT(*) AS Total
-FROM [MonEntreprise].[dbo].[Telco-Customer-Churn]
-GROUP BY 
-    CASE 
-        WHEN tenure < 12 THEN 'New'
-        WHEN tenure < 24 THEN 'Mid-term'
-        ELSE 'Loyal'
-    END,
-    Churn;
-
-
--- Contract type impact
-
-USE [MonEntreprise]
-GO
-
-SELECT 
-    Contract,
-    Churn,
-    COUNT(*) AS Total
-FROM [MonEntreprise].[dbo].[Telco-Customer-Churn]
-GROUP BY Contract, Churn
-ORDER BY Contract;
-
-
-
--- Payment method impact
-
-USE [MonEntreprise]
-GO
-
-SELECT 
-    PaymentMethod,
-    Churn,
-    COUNT(*) AS Total
-FROM [MonEntreprise].[dbo].[Telco-Customer-Churn]
-GROUP BY PaymentMethod, Churn;
-
-
--- Charges analysis
-
-USE [MonEntreprise]
-GO
-
-SELECT
-    Churn,
-    AVG(CAST(MonthlyCharges AS FLOAT)) AS AvgMonthly,
-    AVG(CAST(TotalCharges AS FLOAT)) AS AvgTotal
+-- clients par genre
+SELECT gender, COUNT(*) AS Total
 FROM [dbo].[Telco-Customer-Churn]
-WHERE 
-    TRY_CAST(TotalCharges AS FLOAT) IS NOT NULL
-    AND TRY_CAST(MonthlyCharges AS FLOAT) IS NOT NULL
-GROUP BY Churn;
+GROUP BY gender;
 
+-- clients par type de contrat
+SELECT Contract, COUNT(*) AS Total
+FROM [dbo].[Telco-Customer-Churn]
+GROUP BY Contract;
 
--- 3. Services Analysis
--- Internet service
-USE [MonEntreprise]
-GO
+-- clients avec Fiber optic
+SELECT *
+FROM [dbo].[Telco-Customer-Churn]
+WHERE InternetService = 'Fiber optic';
 
-SELECT 
-    InternetService,
-    Churn,
-    COUNT(*) AS Total
-FROM [MonEntreprise].[dbo].[Telco-Customer-Churn]
-GROUP BY InternetService, Churn;
+-- clients qui ont churn
+SELECT *
+FROM [dbo].[Telco-Customer-Churn]
+WHERE Churn = 'Yes';
 
--- Add-on services
+-- tri par charges mensuelles
+SELECT customerID, MonthlyCharges
+FROM [dbo].[Telco-Customer-Churn]
+ORDER BY CAST(MonthlyCharges AS FLOAT) DESC;
 
-USE [MonEntreprise]
-GO
+-- moyenne charges mensuelles
+SELECT AVG(CAST(MonthlyCharges AS FLOAT)) AS AvgMonthly
+FROM [dbo].[Telco-Customer-Churn];
 
-SELECT 
-    OnlineSecurity,
-    TechSupport,
-    Churn,
-    COUNT(*) AS Total
-FROM [MonEntreprise].[dbo].[Telco-Customer-Churn]
-GROUP BY OnlineSecurity, TechSupport, Churn; 
-
-
--- Phone service
-USE [MonEntreprise]
-GO
-
-SELECT 
-    PhoneService,
-    MultipleLines,
-    Churn,
-    COUNT(*) AS Total
-FROM [MonEntreprise].[dbo].[Telco-Customer-Churn]
-GROUP BY PhoneService, MultipleLines, Churn;
+-- clients seniors
+SELECT SeniorCitizen, COUNT(*) AS Total
+FROM [dbo].[Telco-Customer-Churn]
+GROUP BY SeniorCitizen;
